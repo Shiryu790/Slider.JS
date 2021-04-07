2b61c93708f87db161f1356e585da709dadbbcb0
Amr Abd El Fattah <amrabdelfattah@hotmail.fr>
2021-04-07 20:41:15 +0200

    Slider JS

---
+++ app.js
@@ -0,0 +1,43 @@
+const items         = document.querySelectorAll("img"); 
+const suivant       = document.querySelector (".right"); 
+const precedent     = document.querySelector (".left"); 
+const nbSlide       = items.length; 
+let count           = 0; 
+
+function slideSuivante() {
+    items[count].classList.remove ("active"); 
+
+    if (count < nbSlide - 1) {
+        count++; 
+    }
+    else {
+        count = 0; 
+    }
+
+    items[count].classList.add("active"); 
+}
+suivant.addEventListener("click", slideSuivante); 
+
+function slidePrecedente() {
+    items[count].classList.remove("active"); 
+
+    if (count > 0) {
+        count--; 
+    }
+    else {
+        count = nbSlide -1; 
+    }
+    items[count].classList.add("active"); 
+}
+precedent.addEventListener("click", slidePrecedente); 
+
+function keyPress(e){
+    if(e.keyCode === 37){
+        slidePrecedente(); 
+    }
+    else if(e.keyCode === 39){
+        slideSuivante(); 
+    }
+}
+
+document.addEventListener("keydown", keyPress); 
